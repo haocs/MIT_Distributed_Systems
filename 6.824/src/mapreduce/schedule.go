b49f6628a89b41216b7e73d377e220b0a86812d9
Hao Chen <git@haoc.io>
2017-11-14 01:08:47 -0800

    parallel tasks working

--- 6.824/src/mapreduce/schedule.go
+++ 6.824/src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (Map
@@ -11,6 +14,7 @@ import "fmt"
 // suitable for passing to call(). registerChan will yield all
 // existing registered workers (if any) and new ones as they register.
 //
+// Hao: all workers will be registered each time the schedule() get invoked.
 func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
 	var ntasks int
 	var n_other int // number of inputs (for reduce) or outputs (for map)
@@ -25,12 +29,63 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
 
-	// All ntasks tasks have to be scheduled on workers, and only once all of
-	// them have been completed successfully should the function return.
-	// Remember that workers may fail, and that any given worker may finish
-	// multiple tasks.
-	//
-	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-	//
+	var waitGroup sync.WaitGroup
+	workerChan := initWorkerChan(&registerChan, 10)
+
+	for i:= 0; i < ntasks; i++ {
+		fmt.Printf("Submitting task: %d\n", i)
+		waitGroup.Add(1)
+
+		go func(registerChan chan string, workerChan chan string, jobNum int, mapFile []string) {
+			defer waitGroup.Done()
+
+			populateNewWorkerFromRegisterChan(&registerChan, &workerChan)
+
+			worker := <- workerChan
+			fmt.Printf("Avaialbe worker: %s\n", worker)
+
+			// Do map/reduce
+			var inFile string
+			if phase == mapPhase {
+				inFile = mapFiles[jobNum]
+				//fmt.Printf("file %s\n", inFile)
+			}
+			doTaskArgs := DoTaskArgs{
+				JobName:jobName,
+				File: inFile,
+				Phase: phase,
+				TaskNumber:jobNum,
+				NumOtherPhase: n_other}
+			doTask(worker, doTaskArgs)
+
+			fmt.Printf("Task %d done\n", jobNum)
+
+			//When task finished, add worker back then update waitgroup.
+			workerChan <- worker
+		}(registerChan, workerChan, i, mapFiles)
+	}
+	// wait all submitted tasks to finish
+	waitGroup.Wait()
 	fmt.Printf("Schedule: %v phase done\n", phase)
+}
+
+func initWorkerChan(registerChan *chan string, size int) chan string {
+	workerChan := make(chan string, size)
+	worker := <- *registerChan
+	workerChan <- worker
+	return workerChan
+}
+
+func populateNewWorkerFromRegisterChan(registerChan *chan string, workerChan *chan string) {
+	select {
+	case newWorker := <- *registerChan:
+		fmt.Printf("new worker found: %s\n", newWorker)
+		*workerChan <- newWorker
+	default:
+	}
+}
+
+func doTask(worker string, doTaskArgs DoTaskArgs) bool {
+	rpcName := "Worker.DoTask"
+	return call(worker, rpcName, doTaskArgs, nil)
 }
